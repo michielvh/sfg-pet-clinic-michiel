4dd3e86d4d282fa5aa5980976b91436e03b9ef0e
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-06-23 16:41:09 +0200

    Adding Vet List and updated Controller. closes #15

--- pet-clinic-michiel-web/src/main/java/michiel/springframework/sfgpetclinicmichiel/controllers/VetController.java
+++ pet-clinic-michiel-web/src/main/java/michiel/springframework/sfgpetclinicmichiel/controllers/VetController.java
@@ -1,14 +1,26 @@
 package michiel.springframework.sfgpetclinicmichiel.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import michiel.springframework.sfgpetclinicmichiel.services.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
-        return"vets/index";
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html" })
+    public String listVets(Model model) {
+
+        model.addAttribute("vets",vetService.findAll());
+
+        return "vets/index";
     }
 
 }
