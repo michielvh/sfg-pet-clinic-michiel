ed15cb314cfa0917b2f0e77f1c9ee3fbcbf03798
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-06-26 16:13:27 +0200

    Adding SpecialityService & SpecialityMapService. closes #31

---
+++ pet-clinic-michiel-data/src/main/java/michiel/springframework/sfgpetclinicmichiel/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package michiel.springframework.sfgpetclinicmichiel.services;
+
+import michiel.springframework.sfgpetclinicmichiel.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality ,Long>{
+
+}
