15bc8777d7826fd5874de028b5332854d1b34c36
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-06-29 16:02:05 +0200

    MockMVC tests for OwnerController. closes #51

--- pet-clinic-michiel-web/src/main/java/michiel/springframework/sfgpetclinicmichiel/controllers/OwnerController.java
+++ pet-clinic-michiel-web/src/main/java/michiel/springframework/sfgpetclinicmichiel/controllers/OwnerController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import michiel.springframework.sfgpetclinicmichiel.services.OwnerService;
 
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
---
+++ pet-clinic-michiel-web/src/test/java/michiel/springframework/sfgpetclinicmichiel/controllers/OwnerControllerTest.java
@@ -0,0 +1,74 @@
+package michiel.springframework.sfgpetclinicmichiel.controllers;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import michiel.springframework.sfgpetclinicmichiel.model.Owner;
+import michiel.springframework.sfgpetclinicmichiel.services.OwnerService;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    Set<Owner> owners;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        Owner o1 = new Owner();
+        o1.setId(1l);
+        Owner o2 = new Owner();
+        o2.setId(2l);
+        owners.add(o1);
+        owners.add(o2);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        // goes to controller and does get call to URI "/owners" and checks if it returns 200 code
+        // is(200) == isOk()
+        mockMvc.perform(get("/owners")).andExpect(status().is(200))
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+    @Test
+    void findOwners() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notimplemented"));
+
+        // ownerService is niet gebruikt geweest
+        verifyNoInteractions(ownerService);
+    }
+}
